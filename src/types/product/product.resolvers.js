32851fea0078df6f1ae47f06dcdacc7cffd52fb7
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-01-13 22:02:54 +0100

    mucking arround with resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -10,8 +10,62 @@ const productsTypeMatcher = {
 }
 
 export default {
-  Query: {},
-  Mutation: {},
+  Product: {
+    id(_, value) {
+      return '_id'
+    }
+  },
+  Query: {
+    products() {
+      return Product.find({}).exec()
+    },
+    product(_, args) {
+      return Product.find({ _id: args.id }).exec()
+    }
+  },
+  Mutation: {
+    newProduct(_, args) {
+      Product.create(args.input, (err, product) => {
+        if (err) {
+          throw err
+        }
+        if (!product) {
+          throw new Error('could not make the new product')
+        }
+        return product
+      })
+    },
+    removeProduct(_, args) {
+      Product.findById(args.id, (err, product) => {
+        if (err) {
+          throw err
+        }
+        if (!product) {
+          throw new Error('could not find product to remove')
+        }
+        return product.remove()
+      })
+    },
+    updateProduct(_, args) {
+      Product.findById(args.id, (err, product) => {
+        if (err) {
+          throw err
+        }
+        if (!product) {
+          throw new Error('could not find prod to update the product')
+        }
+        product.update(args, (err, updatedProduct) => {
+          if (err) {
+            throw err
+          }
+          if (!updatedProduct) {
+            throw new Error('could not update the product')
+          }
+          return updatedProduct
+        })
+      })
+    }
+  },
   Product: {
     __resolveType(product) {}
   }
