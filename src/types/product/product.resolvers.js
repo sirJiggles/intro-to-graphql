0481662b825144ac48dabcb1e0457faeee403a85
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-01-15 00:05:34 +0100

    done the lesson 5

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,26 +9,42 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const authCheck = function(ctx) {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+}
+
+const adminCheck = function(ctx) {
+  if (ctx.user.role !== 'admin') {
+    throw new AuthenticationError()
+  }
+}
+
 /** product */
 const product = (_, args, ctx) => {
+  authCheck(ctx)
   return Product.findById(args.id)
     .lean()
     .exec()
 }
 
 const newProduct = (_, args, ctx) => {
-  // use this fake ID for createdBy for now until we talk auth
-  const createdBy = mongoose.Types.ObjectId()
-  return Product.create({ ...args.input, createdBy })
+  authCheck(ctx)
+  adminCheck(ctx)
+  return Product.create({ ...args.input, createdBy: ctx.user })
 }
 
 const products = (_, args, ctx) => {
+  authCheck(ctx)
   return Product.find({})
     .lean()
     .exec()
 }
 
 const updateProduct = (_, args, ctx) => {
+  authCheck(ctx)
+  adminCheck(ctx)
   const update = args.input
   return Product.findByIdAndUpdate(args.id, update, { new: true })
     .lean()
@@ -36,6 +52,8 @@ const updateProduct = (_, args, ctx) => {
 }
 
 const removeProduct = (_, args, ctx) => {
+  authCheck(ctx)
+  adminCheck(ctx)
   return Product.findByIdAndRemove(args.id)
     .lean()
     .exec()
